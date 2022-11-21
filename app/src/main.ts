fa64150ce09ffa8167d4ba088b92bf6da60d1700
Max Niederman <max@maxniederman.com>
2022-11-21 15:44:55 -0800

    feat(app): use Noto Sans

--- app/src/main.ts
+++ app/src/main.ts
@@ -3,6 +3,7 @@ import App from "./App.svelte";
 import "./styles/global.scss";
 import "./styles/reset.scss";
 import "open-color/open-color.css";
+import "@fontsource/noto-sans";
 
 const app = new App({
 	target: document.getElementById("app"),
