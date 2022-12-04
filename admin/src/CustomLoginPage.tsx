cd6eb1cb65bb49e6e32d5e723c3f94ac949dd690
Craftzman7 <zezetta@icloud.com>
2022-12-03 19:58:03 -0800

    feat(admin): add simple user validation

--- admin/src/CustomLoginPage.tsx
+++ admin/src/CustomLoginPage.tsx
@@ -3,14 +3,22 @@ import { Login, LoginForm } from "react-admin";
 import StyledFirebaseAuth from "./StyledFirebaseAuth";
 import firebase from "firebase/compat/app";
 
-// Configure FirebaseUI.
 const uiConfig = {
-	// Popup signin flow rather than redirect flow.
 	signInFlow: "popup",
-	// Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
 	signInSuccessUrl: "#/",
-	// We will display Google and Facebook as auth providers.
 	signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID],
+  callbacks: {
+    signInSuccessWithAuthResult: (result: { credential: any; user: any; }) => {
+      const user = result.user;
+      
+      if (!user.email.endsWith('@pps.net')) {
+        firebase.auth().signOut();
+        return false;
+      }
+
+      return true;
+    }
+  }
 };
 
 const SignInScreen = () => (
