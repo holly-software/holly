b621a8ac270503ec7d0b82804798bd6c516bdcde
Max Niederman <max@maxniederman.com>
2022-12-07 19:44:24 -0800

    fix(app): update app id

--- app/capacitor.config.ts
+++ app/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-	appId: "com.maxniederman.holly",
+	appId: "school.holly.app",
 	appName: "Holly",
 	webDir: "dist",
 	bundledWebRuntime: false,
