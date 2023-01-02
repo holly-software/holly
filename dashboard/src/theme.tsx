650fc9293333025af88546979f70a9012c54f08c
Max Niederman <max@maxniederman.com>
2023-01-02 13:46:34 -0800

    feat(dashboard): load only latin fonts

--- dashboard/src/theme.tsx
+++ dashboard/src/theme.tsx
@@ -6,10 +6,10 @@ import {
 import { LinkProps } from "@mui/material/Link";
 import { createTheme } from "@mui/material";
 
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
+import "@fontsource/roboto/latin-300.css";
+import "@fontsource/roboto/latin-400.css";
+import "@fontsource/roboto/latin-500.css";
+import "@fontsource/roboto/latin-700.css";
 
 const LinkBehavior = React.forwardRef<
 	HTMLAnchorElement,
