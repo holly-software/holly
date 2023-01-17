8229dc86153fe19aa8f6cb4a6000e35ccadf0f2f
Max Niederman <max@maxniederman.com>
2023-01-16 17:51:35 -0800

    feat(app): remove open color dep

--- app/src/main.ts
+++ app/src/main.ts
@@ -2,7 +2,6 @@ import App from "./App.svelte";
 
 import "./styles/global.scss";
 import "./styles/reset.scss";
-import "open-color/open-color.css";
 import "@fontsource/noto-sans";
 
 const app = new App({
