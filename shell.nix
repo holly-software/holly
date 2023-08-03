3eccd2c9ffeaf9e9822f2f39d1aafa29365922b2
Max Niederman <max@maxniederman.com>
2023-08-02 20:16:17 -0700

    chore: fix nix environment

--- shell.nix
+++ shell.nix
@@ -1,5 +1,13 @@
 { pkgs ? import <nixpkgs> { } }:
 
 pkgs.mkShell {
-  packages = with pkgs; [ nodejs-16_x ];
+  packages = with pkgs; [
+    nodejs-16_x
+    nodePackages.pnpm
+
+    temurin-jre-bin
+    android-studio
+  ];
+
+  CAPACITOR_ANDROID_STUDIO_PATH = "${pkgs.android-studio}/bin/android-studio";
 }
