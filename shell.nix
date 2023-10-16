6f279decf3dd631740853b452cb397b4535a34df
Max Niederman <max@maxniederman.com>
2023-10-15 22:36:42 -0700

    build: fix android app builds
    The Android project was compiling to sdk version 32,
    which Capacitor doesn't support.
    Why Capacitor generates the project like this is beyond me.

--- shell.nix
+++ shell.nix
@@ -6,16 +6,27 @@
   }
 }:
 
-let androidComposition = pkgs.androidenv.androidPkgs_9_0;
+let
+  androidComposition =
+    pkgs.androidenv.composeAndroidPackages {
+      platformVersions = [ "29" "32" "33" ];
+      buildToolsVersions = [ "30.0.3" ];
+      abiVersions = [ "x86" "x86_64" ];
+    };
 in
-pkgs.mkShell {
+pkgs.mkShell rec {
   packages = with pkgs; [
     bun
+
+    act
+
+    temurin-bin-17
     android-studio
   ];
 
-  ANDROID_SDK_ROOT = "${androidComposition.androidsdk}/libexec/android-sdk";
-  ANDROID_NDK_ROOT = "${androidComposition.androidsdk}/libexec/android-sdk/ndk-bundle";
+  ANDROID_HOME = "${androidComposition.androidsdk}/libexec/android-sdk";
+  ANDROID_SDK_ROOT = ANDROID_HOME;
+  ANDROID_NDK_ROOT = "${ANDROID_HOME}/ndk-bundle";
 
   CAPACITOR_ANDROID_STUDIO_PATH = "${pkgs.android-studio}/bin/android-studio";
 }
